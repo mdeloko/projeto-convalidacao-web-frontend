5de677df43958750c1f0da1ac9dad7c5e1aab387
Erik G. Coutinho <erikgcoutinho03@gmail.com>
2026-05-11 23:19:07 -0300

    feat(deploy.sh): Script de automação de deploy ao github pages.

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+echo "🚀 Iniciando o Deploy!"
+
+git worktree add ../deploy deploy
+
+echo "📤 Preparando arquivos..."
+
+cp -r ./projeto/pages ../deploy/pages
+cp -r ./projeto/public ../deploy/public
+cp -r ./projeto/index.html ../deploy/index.html
+cp -r ./projeto/style.css ../deploy/style.css
+cp -r ./projeto/index.js ../deploy/index.js
+
+cd ../deploy
+
+echo "💾 Commitando alterações..."
+
+git add .
+git commit -m "Deploy ($(date))"
+git push origin deploy
+
+cd ../Avaliação\ Desenvolvimento\ Frontend
+git worktree remove deploy
+
+rm -rf ../deploy
+git worktree prune
+
+echo "✅ Deploy finalizado com sucesso!"
