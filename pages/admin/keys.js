2c3f6ebeded156083afffd98e3de1ee820bf0ca6
Erik G. Coutinho <erikgcoutinho03@gmail.com>
2026-05-11 23:47:32 -0300

    Deploy (seg 11 mai 2026 23:47:32 -03)

--- pages/admin/keys.js
+++
@@ -1,7 +0,0 @@
-export const KEYS = Object.freeze({
-    KEY_NOME : 'KEY_NOME',
-    KEY_DATA : 'KEY_DATA',
-    KEY_EMAIL : 'KEY_EMAIL',
-    KEY_TIPO : 'KEY_TIPO_CADASTRO',
-    KEY_FRUTA : 'KEY_FRUTA',
-})
