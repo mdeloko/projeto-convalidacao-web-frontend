f4c32bb9c37987e224fb1c156066f5c688d99fa3
Erik G. Coutinho <erikgcoutinho03@gmail.com>
2026-05-12 02:16:33 -0300

    Deploy (ter 12 mai 2026 02:16:33 -03)

--- pages/admin/admin.js
+++ pages/admin/admin.js
@@ -1,73 +1,88 @@
 const enviarButton = document.querySelector('#enviar');
-const clearFiledsButton = document.querySelector('#limpar')
+const clearFiledsButton = document.querySelector('#limpar');
 
 const inputNome = document.querySelector('#nome');
-const inputDataNasc = document.querySelector('#data_nasc');
 const inputEmail = document.querySelector('#email');
-let inputRadioTipoCadastro;
-const inputFruta = document.querySelector('#fruta');
+const dadosDiv = document.querySelector('#dados');
+
+const KEY_LISTA_PESSOAS = 'KEY_LISTA'
+let list;
+
+document.addEventListener('DOMContentLoaded',(e)=>{
+    popularListaVisual();
+})
+
+function atualizaLista(){
+    list = JSON.parse(window.localStorage.getItem(KEY_LISTA_PESSOAS)) || [];
+}
+
+function popularListaVisual(){
+    atualizaLista();
+    while(dadosDiv.firstChild){
+        dadosDiv.removeChild(dadosDiv.firstChild);
+    }
+
+    list.map((val,idx)=>{
+        const newCard = document.createElement('div');
+        newCard.classList.add('card')
+        const name = document.createElement('b');
+        name.innerHTML = val.nome;
+        const email = document.createElement('span');
+        email.innerHTML = val.email;
+        newCard.id = idx;
+        newCard.appendChild(name);
+        newCard.appendChild(email);
+        dadosDiv.appendChild(newCard);
+    })
+}
 
 function logAllInputs(){
-    console.log(inputNome)
-    console.log(inputDataNasc)
-    console.log(inputEmail)
-    console.log(inputRadioTipoCadastro)
-    console.log(inputFruta)
+    console.log(inputNome);
+    console.log(inputEmail);
 }
 
 enviarButton.addEventListener('click',(evt)=>{
     evt.preventDefault();
-    inputRadioTipoCadastro = document.querySelector('input[name="tipo_cadastro"]:checked')
-    if(!inputNome.value || !inputDataNasc.value || !inputEmail.value || !inputRadioTipoCadastro || !inputFruta.value){
+    if(!inputNome.value || !inputEmail.value){
+        let errStr = "Preencha o(s) campo(s) obrigatório(s):\n";
         if(!inputNome.value) {
-            inputNome.focus();
-            window.alert("Preencha o Nome!")
+            errStr += "- Nome;\n"
         }
-        else if(!inputDataNasc.value) {
-            inputDataNasc.focus();
-            window.alert("Preencha a Data de Nascimento!")
+        if(!inputEmail.value) {
+            errStr += "- E-mail;\n"
         }
-        else if(!inputEmail.value) {
-            inputEmail.focus();
-            window.alert("Preencha o E-mail!")
-        }
-        else if(!inputFruta.value) {
-            inputFruta.focus();
-            window.alert("Preencha a Fruta!")
-        }
-        else if(!inputRadioTipoCadastro) {
-            window.alert("Preencha se você é Aluno/Professor!");
-        }
+        window.alert(errStr)
         return
     }else{
         const newPerson = {
             nome: inputNome.value,
-            data_nasc: inputDataNasc.value,
             email: inputEmail.value,
-            tipo: inputRadioTipoCadastro.id,
-            fruta: inputFruta.value
         }
-        let list = JSON.parse(window.localStorage.getItem(KEYS.KEY_LISTA_PESSOAS)) || [];
         list.push(newPerson);
-        window.localStorage.setItem(KEYS.KEY_LISTA_PESSOAS,JSON.stringify(list));
-        console.log("Lista:",list)
-
-        window.alert("Cadastrado com Sucesso! Redirecionando...");
-    }
-})
-
-clearFiledsButton.addEventListener('click',(e)=>{
-    e.preventDefault();
-    inputRadioTipoCadastro = document.querySelector('input[name="tipo_cadastro"]:checked')
-    if(!inputNome || !inputDataNasc || !inputEmail || !inputFruta){
-        window.alert("Erro com os inputs!")
-        return
-    }else{
-        window.alert("Os campos serão limpos.");
+        window.localStorage.setItem(KEY_LISTA_PESSOAS,JSON.stringify(list));
         inputNome.value = '';
-        inputDataNasc.value = '';
         inputEmail.value = '';
-        if (inputRadioTipoCadastro) inputRadioTipoCadastro.checked = false;
-        inputFruta.value = '';
+        popularListaVisual();
     }
 })
+
+document.querySelector('#limparLocalStorage').addEventListener('click',(e)=>{
+    e.preventDefault()
+    window.localStorage.clear()
+    popularListaVisual()
+})
+// clearFiledsButton.addEventListener('click',(e)=>{
+//     e.preventDefault();
+//     inputRadioTipoCadastro = document.querySelector('input[name="tipo_cadastro"]:checked')
+//     if(!inputNome || !inputDataNasc || !inputEmail || !inputFruta){
+//         window.alert("Erro com os inputs!")
+//         return
+//     }else{
+//         window.alert("Os campos serão limpos.");
+//         inputNome.value = '';
+//         inputDataNasc.value = '';
+//         inputEmail.value = '';
+//         if (inputRadioTipoCadastro) inputRadioTipoCadastro.checked = false;
+//         inputFruta.value = '';
+//     }
+// })
